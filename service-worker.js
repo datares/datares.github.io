08e6d6c5717f6ce6fc1311cd4a6f9a11aed36e80
Agrim Gupta <agrimgupta@Agrims-MacBook-Pro.local>
2022-03-28 01:19:02 +0530

    updated research link

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.30b8d39e109862eae2a1aec1cc2adcae.js"
+  "/precache-manifest.32dd9997ac684b3709bdf9367a6b33ac.js"
 );
 
 self.addEventListener('message', (event) => {
