c41f935bbf46eef048911bbe453cab657e0c37d8
Colin Curtis <colinpcurtis826@ucla.edu>
2022-03-28 11:11:18 -0700

    update latest data blog articles

---
+++ precache-manifest.1c0b34f89ffd6662ed5163269ad289e3.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3491180dbbcd8f39862ef93d3360916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be7b03c3d6fb3eb91b61",
+    "url": "/static/css/2.7df41cd5.chunk.css"
+  },
+  {
+    "revision": "350098539aeecd53ad6e",
+    "url": "/static/css/main.b8ae677e.chunk.css"
+  },
+  {
+    "revision": "be7b03c3d6fb3eb91b61",
+    "url": "/static/js/2.f767c81e.chunk.js"
+  },
+  {
+    "revision": "660b51dc7bed49ff6652ad5ac02ed206",
+    "url": "/static/js/2.f767c81e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "350098539aeecd53ad6e",
+    "url": "/static/js/main.706d2648.chunk.js"
+  },
+  {
+    "revision": "a320a65f955531ba88de",
+    "url": "/static/js/runtime-main.c250c0ac.js"
+  },
+  {
+    "revision": "531ba590571e2628f4c52155c60f9e25",
+    "url": "/static/media/andrew.531ba590.jpg"
+  },
+  {
+    "revision": "728cc4e5dc3fd0e6f26e41523538423c",
+    "url": "/static/media/anika.728cc4e5.jpg"
+  },
+  {
+    "revision": "3553553f8f9ace14a907bd2bfe1337f5",
+    "url": "/static/media/avishek.3553553f.JPG"
+  },
+  {
+    "revision": "9d7a54fcf5fd00a7449dcd120ffb6237",
+    "url": "/static/media/background.9d7a54fc.svg"
+  },
+  {
+    "revision": "b3e1b1024cbe2c6bfbfcf0bb034db737",
+    "url": "/static/media/background_flipped.b3e1b102.svg"
+  },
+  {
+    "revision": "65aa4bf9f69a23af825a75aba4d808f2",
+    "url": "/static/media/basepaws.65aa4bf9.jpg"
+  },
+  {
+    "revision": "22fe9315014912fb989dd414058cd9ed",
+    "url": "/static/media/blog_image.22fe9315.svg"
+  },
+  {
+    "revision": "6513a6f26551cc2da03c1f6d49cbfccb",
+    "url": "/static/media/bottom_wave.6513a6f2.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "95a59180b9fa89a0196f025b47170082",
+    "url": "/static/media/brandon.95a59180.JPG"
+  },
+  {
+    "revision": "9ce806c97a138da9c3ce3d41efc80778",
+    "url": "/static/media/colin.9ce806c9.jpeg"
+  },
+  {
+    "revision": "9ef10d0b71bd6ceac17b144c3b710fe8",
+    "url": "/static/media/consulting_image.9ef10d0b.svg"
+  },
+  {
+    "revision": "e60f360ca4d58547c5345dcb4b80d484",
+    "url": "/static/media/derek.e60f360c.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "06d47c406c8f0fe72de6fb210a193358",
+    "url": "/static/media/irsyad.06d47c40.jpg"
+  },
+  {
+    "revision": "8369869fee5c3476165879a725a3d0a0",
+    "url": "/static/media/isha.8369869f.png"
+  },
+  {
+    "revision": "07d1b0cfc5e84bb0e227205a0bbeddc5",
+    "url": "/static/media/ishaan.07d1b0cf.png"
+  },
+  {
+    "revision": "6d588522441cca5085524d803fb92957",
+    "url": "/static/media/ivan.6d588522.jpg"
+  },
+  {
+    "revision": "bc2a5cd423af7f5a63ef42191a7334f0",
+    "url": "/static/media/justin.bc2a5cd4.jpeg"
+  },
+  {
+    "revision": "e6070a50b0936db02a883f0f8c3f896d",
+    "url": "/static/media/karina.e6070a50.jpeg"
+  },
+  {
+    "revision": "416d2c1de1a2b25c30f53b9e12de868d",
+    "url": "/static/media/logo.416d2c1d.svg"
+  },
+  {
+    "revision": "0578bcd79c3076ddf9bfe0b3df928f23",
+    "url": "/static/media/logos.0578bcd7.png"
+  },
+  {
+    "revision": "c0e7ad3e5cca5bdee4d2f8094fa907f6",
+    "url": "/static/media/madison.c0e7ad3e.jpg"
+  },
+  {
+    "revision": "7ca0cb1da606df832fe5aa5bcc74849e",
+    "url": "/static/media/nav_wave.7ca0cb1d.svg"
+  },
+  {
+    "revision": "0a7b9962b78124d5f02bcf78f57346af",
+    "url": "/static/media/number_wave.0a7b9962.svg"
+  },
+  {
+    "revision": "d0ad558135ac9875a6146ebd47d68681",
+    "url": "/static/media/orange_bar.d0ad5581.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "4e844611ebb9f3a7b019563ffa290381",
+    "url": "/static/media/raymond.4e844611.jpg"
+  },
+  {
+    "revision": "b775d0597b21f18f1488894592cc22b4",
+    "url": "/static/media/research_image.b775d059.svg"
+  },
+  {
+    "revision": "d8729285394d385302c929418f5d6ec5",
+    "url": "/static/media/sausy.d8729285.jpeg"
+  },
+  {
+    "revision": "71c2fe5e1c0daa23ca8a7ff82e53fb72",
+    "url": "/static/media/taylor.71c2fe5e.jpeg"
+  },
+  {
+    "revision": "18d1efacc0ac71a2be8cc7d010447b0f",
+    "url": "/static/media/team_wave.18d1efac.svg"
+  },
+  {
+    "revision": "917a605bba8e03820eaa8a3b585ec628",
+    "url": "/static/media/team_wave.917a605b.svg"
+  },
+  {
+    "revision": "67bcf95e2ace8c60bb7664fc7a31e785",
+    "url": "/static/media/ucla-athletics.67bcf95e.png"
+  },
+  {
+    "revision": "25e1976ebe954b87bae295d28b79a23d",
+    "url": "/static/media/wave.25e1976e.svg"
+  }
+]);
